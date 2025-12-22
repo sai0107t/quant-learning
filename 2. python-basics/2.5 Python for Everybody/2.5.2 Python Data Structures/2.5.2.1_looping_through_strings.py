6440372a080480b81a8b1af83759bf4f2a6817f2
sai0107t <sai0107.t@gmail.com>
2025-12-23 05:08:39 +0530

    added explanations to code

--- 2. python-basics/2.5 Python for Everybody/2.5.2 Python Data Structures/2.5.2.1_looping_through_strings.py
+++ 2. python-basics/2.5 Python for Everybody/2.5.2 Python Data Structures/2.5.2.1_looping_through_strings.py
@@ -3,11 +3,11 @@ fruit = 'banana'
 index = 0
 while index < len(fruit):
     letter = fruit[index]
-    print(index, letter)
+    print(index, letter)# you can avoid giving spaces after comma also as it will fetch the same result even if you avoid sapces after comma.
     index = index + 1   
 # Using a for loop to do the same thing
 fruit = 'banana'
-for letter in fruit:
+for letter in fruit: # when using for letter will be defined automatically by for loop and if we are using in function, it tells that it is looking for each letter in the fruit.
     print(letter)
 # Using a for loop with the range function to get the index
 fruit = 'banana'
---
+++ 2. python-basics/2.5 Python for Everybody/2.5.2 Python Data Structures/practice_01.py
@@ -0,0 +1,6 @@
+fruit = 'banana'
+a=0
+for i in fruit:
+    print(i,a)
+    a=a+1
+
