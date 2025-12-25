c8da6f8917b5beceb7c8f2a0a9d8b17a5af552d7
sai0107t <sai0107.t@gmail.com>
2025-12-25 12:13:04 +0530

    new practice files added

--- 2. python-basics/2.5 Python for Everybody/2.5.2 Python Data Structures/practice_02.py
+++ 2. python-basics/2.5 Python for Everybody/2.5.2 Python Data Structures/practice_02.py
@@ -1,3 +1,20 @@
-# use the file name mbox-short.txt as the file name
+# write a program that prompts for a file name, then opens that file and read throught he file, looking for lines of the form:
+# X-DSPAM-Confidence: 0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+# When you are testing below enter mbox-short.txt as the file name.
 
-fname=
+fname=input("Enter the file name: ")
+fhand=open(fname)
+count=0
+total=0.0
+
+for line in fhand:
+    line=line.rstrip()
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    count=count+1
+    colonpos=line.find(":")
+    number=float(line[colonpos+1:])
+    total=total+number
+print("Average spam confidence:", total/count)
